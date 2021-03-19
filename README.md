287281751455014d7fa48d0106d180c3649e1e36
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2021-03-18 21:53:54 -0400

    first commit: Added calendar, countdown, text date time with style

---
+++ README.md
@@ -0,0 +1,39 @@
+This is a package for StealthSeminar users for showing webinar date and time, countdown and calendar .
+
+Add this to the header section of your page
+
+```
+url coming
+```
+
+This package is dependent on DayJS heavily. These are the dependencies:
+
+```
+<script src="https://unpkg.com/dayjs@1.10.4/dayjs.min.js"></script>
+
+<script src="https://unpkg.com/dayjs@1.10.4/plugin/utc.js"></script>
+
+<script src="https://unpkg.com/dayjs@1.10.4/plugin/timezone.js"></script>
+
+<script src="https://unpkg.com/dayjs@1.10.4/plugin/advancedFormat.js"></script>
+```
+
+Make sure to put these before the end of the body section.
+
+And then add this package.
+
+```
+url
+```
+
+Now you can use these
+```
+<!-- for showing text date time -->
+<div class="ss-datetime"></div>
+
+<!-- for showing calendar date time -->
+<div class="ss-calendar"></div>
+
+<!-- for showing countdown -->
+<div class="ss-countdown"></div>
+```
---
+++ viewer-information.js
@@ -0,0 +1,216 @@
+let webinar = {
+  spinnerColor: "#000",
+  shortId: "",
+  textDateformat: "MMM D @ h:mmA z",
+  selector: {
+    textDate: ".ss-datetime",
+    calendar: ".ss-calendar",
+    calendarContainer: ".ss-calendar__container",
+    calendarContainerLeft: ".ss-calendar__container-left",
+    calendarContainerRight: ".ss-calendar__container-right",
+    calendarDate: ".ss-calendar__date",
+    calendarDay: ".ss-calendar__day",
+    calendarWeekdayMonth: ".ss-calendar__weekday-month",
+    calendarTime: ".ss-calendar__time",
+    countdown: ".ss-countdown",
+    countdownDays: ".ss-countdown__days",
+    countdownHours: ".ss-countdown__hours",
+    countdownMinutes: ".ss-countdown__minutes",
+    countdownSeconds: ".ss-countdown__seconds",
+  },
+  spinner: function () {
+    return `<svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a"><stop stop-color=${this.spinnerColor} stop-opacity="0" offset="0%"/><stop stop-color=${this.spinnerColor} stop-opacity=".631" offset="63.146%"/><stop stop-color=${this.spinnerColor} offset="100%"/></linearGradient></defs><g fill="none" fill-rule="evenodd"><g transform="translate(1 1)"><path d="M36 18c0-9.94-8.06-18-18-18" id="Oval-2" stroke="url(#a)" stroke-width="2"><animateTransform attributeName="transform" type="rotate" from="0 18 18"  to="360 18 18"  dur="0.9s" repeatCount="indefinite" /></path><circle fill=${this.spinnerColor} cx="36" cy="18" r="1"> <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18"  dur="0.9s" repeatCount="indefinite" /></circle></g></g></svg>`;
+  },
+  textDateContainers: function () {
+    return document.querySelectorAll(this.selector.textDate);
+  },
+  countdownContainers: function () {
+    return document.querySelectorAll(this.selector.countdown);
+  },
+  calendarContainers: function () {
+    return document.querySelectorAll(this.selector.calendar);
+  },
+  loadContainers: function () {
+    const self = this;
+    this.textDateContainers() &&
+      this.textDateContainers().forEach(function (container) {
+        return (container.innerHTML = self.spinner());
+      });
+    this.countdownContainers() &&
+      this.countdownContainers().forEach(function (container) {
+        return (container.innerHTML = self.spinner());
+      });
+
+    this.calendarContainers() &&
+      this.calendarContainers().forEach(function (container) {
+        return (container.innerHTML = self.spinner());
+      });
+  },
+  localTimezone: function () {
+    dayjs.extend(window.dayjs_plugin_utc);
+    dayjs.extend(window.dayjs_plugin_timezone);
+    dayjs.extend(window.dayjs_plugin_advancedFormat);
+    return dayjs.tz.guess();
+  },
+  apiUrl: function () {
+    return `https://api.joinnow.live/webinars/${
+      this.shortId
+    }/registration-information?timezone=${this.localTimezone()}`;
+  },
+  formattedDateTime: function (time, textDateformat) {
+    return dayjs(time).tz(this.localTimezone()).format(textDateformat);
+  },
+  getTimeRemaining: function (endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  },
+  updateClock: function (
+    endtime,
+    daysSpan,
+    hoursSpan,
+    minutesSpan,
+    secondsSpan
+  ) {
+    const time = this.getTimeRemaining(endtime);
+    daysSpan.innerHTML = ("0" + time.days).slice(-2);
+    hoursSpan.innerHTML = ("0" + time.hours).slice(-2);
+    minutesSpan.innerHTML = ("0" + time.minutes).slice(-2);
+    secondsSpan.innerHTML = ("0" + time.seconds).slice(-2);
+
+    if (time.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  },
+  calendarNode: function (req) {
+    const upcomingTimes = JSON.parse(req).upcoming_times;
+    const self = this;
+    this.calendarContainers().forEach(function (clock) {
+      clock.innerHTML = ""; // destroy current spinner
+      upcomingTimes.forEach(function (upcomingTime) {
+        clock.innerHTML += `<div class=${
+          self.selector.calendarContainer.split(".")[1]
+        }><div class=${
+          self.selector.calendarContainerLeft.split(".")[1]
+        }><span class=${
+          self.selector.calendarDate.split(".")[1]
+        }>${self.formattedDateTime(upcomingTime, "MMM")}</span>
+        <span class=${
+          self.selector.calendarDay.split(".")[1]
+        }>${self.formattedDateTime(upcomingTime, "D")}</span></div>
+        <div class=${
+          self.selector.calendarContainerRight.split(".")[1]
+        }><span class=${
+          self.selector.calendarWeekdayMonth.split(".")[1]
+        }>${self.formattedDateTime(upcomingTime, "dddd: MMMM D")}</span>
+        <span class=${
+          self.selector.calendarTime.split(".")[1]
+        }>${self.formattedDateTime(
+          upcomingTime,
+          "hh:mm A z"
+        )}</span></div></div>`;
+      });
+    });
+  },
+  countdownNode: function (req) {
+    const endtime = JSON.parse(req).upcoming_times[0];
+    const self = this;
+    this.countdownContainers().forEach(function (clock) {
+      clock.innerHTML = `<span class=${
+        self.selector.countdownDays.split(".")[1]
+      }></span>:<span class=${
+        self.selector.countdownHours.split(".")[1]
+      }>:</span>:<span
+      class=${
+        self.selector.countdownMinutes.split(".")[1]
+      }></span>:<span class=${
+        self.selector.countdownSeconds.split(".")[1]
+      }></span>`;
+      const daysSpan = document.querySelector(self.selector.countdownDays);
+      const hoursSpan = document.querySelector(self.selector.countdownHours);
+      const minutesSpan = document.querySelector(
+        self.selector.countdownMinutes
+      );
+      const secondsSpan = document.querySelector(
+        self.selector.countdownSeconds
+      );
+      self.updateClock(endtime, daysSpan, hoursSpan, minutesSpan, secondsSpan);
+      setInterval(
+        self.updateClock.bind(self),
+        1000,
+        endtime,
+        daysSpan,
+        hoursSpan,
+        minutesSpan,
+        secondsSpan
+      );
+    });
+  },
+  textNode: function (req) {
+    const upcomingTime = JSON.parse(req).upcoming_times[0];
+    const self = this;
+    this.textDateContainers().forEach(function (container) {
+      container.innerHTML = self.formattedDateTime(
+        upcomingTime,
+        self.textDateformat
+      );
+    });
+  },
+  errorNode: function () {
+    this.textDateContainers() &&
+      this.textDateContainers().forEach(function (container) {
+        container.innerHTML = "Invalid ID";
+      });
+    this.countdownContainers() &&
+      this.countdownContainers().forEach(function (container) {
+        container.innerHTML = "Invalid ID";
+      });
+
+    this.calendarContainers() &&
+      this.calendarContainers().forEach(function (container) {
+        container.innerHTML = "Invalid ID";
+      });
+  },
+  request: function (funcs, err) {
+    if (this.shortId === "") {
+      err();
+      return;
+    }
+    const request = new XMLHttpRequest();
+    request.open("GET", this.apiUrl());
+    request.send();
+    request.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        // see if the request is ready
+        if (this.status === 200) {
+          funcs.forEach(function (func) {
+            return func(request.responseText);
+          });
+        } else {
+          err();
+        }
+      }
+    };
+  },
+};
+
+// bind this to the webinar to fix scopes
+
+let textNode = webinar.textNode.bind(webinar);
+let errorNode = webinar.errorNode.bind(webinar);
+let countdownNode = webinar.countdownNode.bind(webinar);
+let calendarNode = webinar.calendarNode.bind(webinar);
+
+// find the element and show a loading spinner until the following loads
+
+// webinar.request(textNode, errorNode);
