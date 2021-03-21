3337791742c91fd53114e9986f0b7640585ac03d
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2021-03-21 10:25:53 -0400

    changed countdown layout

--- viewer-information.js
+++ viewer-information.js
@@ -2,6 +2,12 @@ let webinar = {
   spinnerColor: "#000",
   shortId: "",
   textDateformat: "MMM D @ h:mmA z",
+  countDownLabels: {
+    days: "Days",
+    hours: "Hours",
+    minutes: "Minutes",
+    seconds: "Seconds",
+  },
   selector: {
     textDate: ".ss-datetime",
     calendar: ".ss-calendar",
@@ -13,6 +19,12 @@ let webinar = {
     calendarWeekdayMonth: ".ss-calendar__weekday-month",
     calendarTime: ".ss-calendar__time",
     countdown: ".ss-countdown",
+    countdownDaysContainer: ".ss-countdown__days-container",
+    countdownHoursContainer: ".ss-countdown__hours-container",
+    countdownMinutesContainer: ".ss-countdown__minutes-container",
+    countdownSecondsContainer: ".ss-countdown__seconds-container",
+    countDownSeparator: ".ss-countdown__separator",
+    countDownLabel: ".ss-countdown__label",
     countdownDays: ".ss-countdown__days",
     countdownHours: ".ss-countdown__hours",
     countdownMinutes: ".ss-countdown__minutes",
@@ -20,6 +32,41 @@ let webinar = {
   },
   spinner: function () {
     return `<svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a"><stop stop-color=${this.spinnerColor} stop-opacity="0" offset="0%"/><stop stop-color=${this.spinnerColor} stop-opacity=".631" offset="63.146%"/><stop stop-color=${this.spinnerColor} offset="100%"/></linearGradient></defs><g fill="none" fill-rule="evenodd"><g transform="translate(1 1)"><path d="M36 18c0-9.94-8.06-18-18-18" id="Oval-2" stroke="url(#a)" stroke-width="2"><animateTransform attributeName="transform" type="rotate" from="0 18 18"  to="360 18 18"  dur="0.9s" repeatCount="indefinite" /></path><circle fill=${this.spinnerColor} cx="36" cy="18" r="1"> <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18"  dur="0.9s" repeatCount="indefinite" /></circle></g></g></svg>`;
+  },
+  countdownContainer: function () {
+    return `<div class=${
+      this.selector.countdownDaysContainer.split(".")[1]
+    }><span class=${
+      this.selector.countdownDays.split(".")[1]
+    }>00</span><span class=${this.selector.countDownLabel.split(".")[1]}>${
+      this.countDownLabels.days
+    }</span></div>
+
+    
+    <div class=${
+      this.selector.countdownHoursContainer.split(".")[1]
+    }><span class=${
+      this.selector.countdownHours.split(".")[1]
+    }>00</span><span class=${this.selector.countDownLabel.split(".")[1]}>${
+      this.countDownLabels.hours
+    }</span></div>
+
+    <div class=${
+      this.selector.countdownMinutesContainer.split(".")[1]
+    }><span class=${
+      this.selector.countdownMinutes.split(".")[1]
+    }>00</span><span class=${this.selector.countDownLabel.split(".")[1]}>${
+      this.countDownLabels.minutes
+    }</span></div>
+
+    <div class=${
+      this.selector.countdownSecondsContainer.split(".")[1]
+    }><span class=${
+      this.selector.countdownSeconds.split(".")[1]
+    }>00</span><span class=${this.selector.countDownLabel.split(".")[1]}>${
+      this.countDownLabels.seconds
+    }</span></div>
+    `;
   },
   textDateContainers: function () {
     return document.querySelectorAll(this.selector.textDate);
@@ -80,17 +127,21 @@ let webinar = {
     daysSpan,
     hoursSpan,
     minutesSpan,
-    secondsSpan
+    secondsSpan,
+    timeInterval
   ) {
     const time = this.getTimeRemaining(endtime);
+
+    if (time.total <= 0) {
+      clearInterval(timeInterval);
+      this.countdownContainer();
+      return;
+    }
+
     daysSpan.innerHTML = ("0" + time.days).slice(-2);
     hoursSpan.innerHTML = ("0" + time.hours).slice(-2);
     minutesSpan.innerHTML = ("0" + time.minutes).slice(-2);
     secondsSpan.innerHTML = ("0" + time.seconds).slice(-2);
-
-    if (time.total <= 0) {
-      clearInterval(timeinterval);
-    }
   },
   calendarNode: function (req) {
     const upcomingTimes = JSON.parse(req).upcoming_times;
@@ -126,16 +177,7 @@ let webinar = {
     const endtime = JSON.parse(req).upcoming_times[0];
     const self = this;
     this.countdownContainers().forEach(function (clock) {
-      clock.innerHTML = `<span class=${
-        self.selector.countdownDays.split(".")[1]
-      }></span>:<span class=${
-        self.selector.countdownHours.split(".")[1]
-      }>:</span>:<span
-      class=${
-        self.selector.countdownMinutes.split(".")[1]
-      }></span>:<span class=${
-        self.selector.countdownSeconds.split(".")[1]
-      }></span>`;
+      clock.innerHTML = self.countdownContainer();
       const daysSpan = document.querySelector(self.selector.countdownDays);
       const hoursSpan = document.querySelector(self.selector.countdownHours);
       const minutesSpan = document.querySelector(
@@ -144,8 +186,7 @@ let webinar = {
       const secondsSpan = document.querySelector(
         self.selector.countdownSeconds
       );
-      self.updateClock(endtime, daysSpan, hoursSpan, minutesSpan, secondsSpan);
-      setInterval(
+      let timeInterval = setInterval(
         self.updateClock.bind(self),
         1000,
         endtime,
@@ -153,6 +194,14 @@ let webinar = {
         hoursSpan,
         minutesSpan,
         secondsSpan
+      );
+      self.updateClock(
+        endtime,
+        daysSpan,
+        hoursSpan,
+        minutesSpan,
+        secondsSpan,
+        timeInterval
       );
     });
   },
